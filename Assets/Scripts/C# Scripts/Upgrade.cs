f95f114a4ac91d946631b6a002adc7076a67ebce
TRUZijunChen <benjamin9527@icloud.com>
2019-04-09 21:14:36 -0700

    updated UI

--- Assets/Scripts/C# Scripts/Upgrade.cs
+++ Assets/Scripts/C# Scripts/Upgrade.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 
 public class Upgrade : MonoBehaviour
 {
+    GameObject[] allObjects;
+    List<GameObject> lvl1Cows = new List<GameObject>();
+    List<GameObject> lvl1Pigs = new List<GameObject>();
+
+    Text lvl1CowText;
+    Text lvl2PigText;
+
+
+    public CurrencySystem curr;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,91 +25,66 @@ public class Upgrade : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        allObjects = Object.FindObjectsOfType<GameObject>(); //get all gameobjects
 
-    public void upgradeAnimal(GameObject animalToUpgrade)
-    {
-        //check if already upgraded
-        //if (upgraded)
-        //{
-
-            //upgrade cow
-            if (animalToUpgrade.name == "Cow")
-            {
-
-                //subtract money from user
-                //if user has enough money, subtract from total and upgrade cow
-                if (GetComponent<CurrencySystem>().subtractMoney(80)) //returns boolean
-                {
-                    //replace cow with larger cow
-                    //Update user money
-                }
-                //if user does not have enough money, let user know and dont subtract anything
-                else
-                {
-                    //display message "Not enough money to upgrade"
-                }
-            }
-            //upgrade chicken
-            else if (animalToUpgrade.name == "Chicken")
-            {
-                //subtract money from user
-                //if user has enough money, subtract from total and upgrade chicken
-                if (GetComponent<CurrencySystem>().subtractMoney(80))
-                {
-                    //replace chicken with larger chicken
-                    //Update user money
-                }
-                //if user does not have enough money, let user know and dont subtract anything
-                else
-                {
-                    //display message "Not enough money to upgrade"
-                }
-            }
-            //upgrade pig
-            else if (animalToUpgrade.name == "Pig")
-            {
-                //subtract money from user
-                //if user has enough money, subtract from total and upgrade pig
-                if (GetComponent<CurrencySystem>().subtractMoney(80))
-                {
-                    //replace pig with larger pig
-                    //Update user money
-                }
-                //if user does not have enough money, let user know and dont subtract anything
-                else
-                {
-                    //display message "Not enough money to upgrade"
-                }
-            }
-        //}
-    }
-
-    public void upgradeCrop(GameObject cropToUpgrade)
-    {
-        //check if upgraded before upgrading
-        //subtract money from user
-        //if user has enough money, subtract from total and upgrade crop
-    }
-
-    public void upgradeBarn(GameObject barn)
-    {
-        Vector3 barnPosition = new Vector3(25.69f, 2.77f, 11.57f);
-        //subtract money from user
-        //if user has enough money, subtract from total and upgrade barn
-        if (GetComponent<CurrencySystem>().subtractMoney(200))
+        foreach(GameObject go in allObjects) //for each game object
         {
-            //remove Barn
-            Destroy(barn);
-            //create new barn lvl2
-            Instantiate(Resources.Load("BarnLv2"), barnPosition, Quaternion.identity);
+            if(go.name.Contains("Lv1Cow")) //if name of object contains "Lv1Cow"
+            {
+                lvl1Cows.Add(go); //add to cow list
+            }
 
+            if(go.name.Contains("Lv1Pig")) //if name of object contains "Lv1Pig"
+            {
+                lvl1Pigs.Add(go); //add to pig list
+            }
+        }
+
+        lvl1CowText.text = "Level 1 Cows: " + lvl1Cows.Count; //adjust total lvl1 cows
+        lvl2PigText.text = "Level 1 Pigs: " + lvl1Pigs.Count; //adjust total lvl1 pigs
+
+    }
+
+    public void upgradeCow()
+    {
+        //check if there are any lvl1 cows to upgrade
+        if (lvl1Cows.Count > 0)
+        {
+            //retreive any level 1 cow
+            GameObject cowToUpgrade = lvl1Cows[0];
+            //retrieve position of object
+            //Vector3 positionOfOldCow = cowToUpgrade.Transform.position;
+            //delete old cow
+            Destroy(cowToUpgrade);
+            //place new cow in spot of old one
+            //Instantiate(Resources.Load("Lv2Cow"), positionOfOldCow, Quaternion.identity);
+            //subtract money from user total
         }
         else
         {
-            //not enough money to upgrade
+            //no cows to upgrade
         }
-            
+        
+
+    }
+
+    public void upgradePig()
+    {
+        if (lvl1Pigs.Count > 0)
+        {
+            //retreive any level 1 pig
+            GameObject pigToUpgrade = lvl1Pigs[0];
+            //retrieve position of object
+            //Vector3 positionOfOldPig = pigToUpgrade.Transform.position;
+            //delete old cow
+            Destroy(pigToUpgrade);
+            //place new cow in spot of old one
+            //Instantiate(Resources.Load("Lv2Pig"), positionOfOldPig, Quaternion.identity);
+            //subtract money form user total
+        }
+        else
+        {
+            //no pigs to upgrade
+        }
     }
 }
